d818cbf9bb6952acf8f51a5027f2d2eaf1393a60
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-06-21 00:43:20 -0300

    Organizando a renderização da listagem

--- script.js
+++ script.js
@@ -34,3 +34,30 @@ document.addEventListener('DOMContentLoaded', function() {
         renderBookList();
         form.reset();
     });
+
+    // Evento de alteração da opção de classificação
+    sortBySelect.addEventListener('change', function() {
+        renderBookList();
+    });
+
+    // Função para exibir os elementos da lista de livros
+    function showBookListElements() {
+        bookListContainer.style.display = 'block'; // Exibe o container da lista de livros
+        document.querySelector('h2.listagem').style.display = 'block'; // Exibe o título da lista
+        document.querySelector('p').style.display = 'block'; // Exibe a mensagem de instrução
+    }
+
+    // Função para renderizar a lista de livros
+    function renderBookList() {
+        const sortBy = sortBySelect.value; // Obtém a opção de classificação selecionada
+        let sortedBooks = [...books]; // Cria uma cópia do array de livros
+
+        // Ordena os livros com base na opção de classificação selecionada
+        if (sortBy === 'title') {
+            sortedBooks.sort((a, b) => a.title.localeCompare(b.title));
+        } else if (sortBy === 'publication-date') {
+            sortedBooks.sort((a, b) => new Date(a.publicationDate) - new Date(b.publicationDate));
+        }
+
+        const bookListBody = document.getElementById('book-list-body');
+        bookListBody.innerHTML = '';
