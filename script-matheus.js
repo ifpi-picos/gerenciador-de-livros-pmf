e880d500fad5d35e3f357517a9deb5db5d713c24
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-06-28 20:30:48 -0300

    Criando uma função p/ exibir o menu do gerenciador

--- script-matheus.js
+++ script-matheus.js
@@ -2,3 +2,14 @@
 const fs = require('fs');
 
 let biblioteca = []; // Armazenar os livros cadastrados.
+
+// Função para exibir o menu de opções do Gerenciador de livros
+function exibirMenu() {
+  console.log('======== Gerenciador de livros =======');
+  console.log('1. Cadastrar livro');
+  console.log('2. Listar livros');
+  console.log('3. Remover livro');
+  console.log('4. Alterar detalhes do livro');
+  console.log('5. Sair');
+  console.log('===========================\n');
+}
